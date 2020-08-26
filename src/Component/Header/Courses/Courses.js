3035dbbca87b0bba05b75ecbd6324bd385f5c66f
shabnam-priyanka <66578322+shabnam-priyanka@users.noreply.github.com>
2020-08-26 18:30:33 -0400

    react

---
+++ src/Component/Header/Courses/Courses.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Fakedata from '../../../Fakedata/data'
+
+
+const Courses = () => {
+    const data = Fakedata;
+    console.log(data);
+    return (
+        <div>
+       
+        </div>
+    );
+};
+
+export default Courses;
